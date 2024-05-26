6d1f2301aae212e273a1eb58ac94fefd83c91ff1
Hunter G <hunterg@pop-os.localdomain>
2024-05-25 19:59:20 -0500

    Created the first the upper right of the nav ribbon with a main menu and main icon

--- jtp-client/src/components/NavigationRibbon.vue
+++ jtp-client/src/components/NavigationRibbon.vue
@@ -1,7 +1,8 @@
 <template>
-    <nav class="text-white p-2">
-        <svg class="w-12 h-12 mx-1 p-3" focusable="false" viewBox="0 0 24 24">
+    <nav class="flex items-center p-2">
+        <svg class="w-12 h-12 mx-1 p-3 rounded-full hover:bg-stone-200" focusable="false" viewBox="0 0 24 24">
             <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill="#5f6368"></path>
         </svg>
+        <h1 class="text-xl font-header font-medium text-zinc-600">Transpiler</h1>
     </nav>
 </template>
