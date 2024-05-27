1baaed89377bb9aaf15064cfd81146658a1c4eef
Hunter G <hunterg@pop-os.localdomain>
2024-05-26 19:44:13 -0500

    Finished the main menu hover feature which was surprisingly complicated

--- jtp-client/src/components/NavigationRibbon.vue
+++ jtp-client/src/components/NavigationRibbon.vue
@@ -1,10 +1,10 @@
 <template>
     <nav class="flex items-center p-2">
-        <div id="main-menu-tool-tip">
-            <svg class="w-12 h-12 mx-1 p-3 rounded-full hover:bg-stone-200" focusable="false" viewBox="0 0 24 24">
+        <div>
+            <svg class="w-12 h-12 mx-1 p-3 hover:bg-stone-200 rounded-full" focusable="false" viewBox="0 0 24 24">
                 <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill="#5f6368"></path>
             </svg>
-            <span id="tool-tip" class="text-sm text-white text-center font-medium bg-zinc-500 rounded-md py-px px-1 mt-1 w-20 z-10 invisible absolute">
+            <span class="text-sm bg-zinc-500 rounded-md py-px px-1 mt-1 w-20">
                 Main menu
             </span>
         </div>
@@ -12,11 +12,22 @@
     </nav>
 </template>
 
-<style>
+<style scoped>
 
-#main-menu-tool-tip:hover #tool-tip {
+span {
+    color: white;
+    text-align: center;
+    font-weight: 500;
+    visibility: hidden;
+    position: absolute;
+    z-index: 10;
+}
+
+svg:hover + span {
     visibility: visible;
 }
+
+
 
 </style>
 
