9f3e417398cda34f8da1aaee9eb1e72431ebf1c4
Hunter G <hunterg@pop-os.localdomain>
2024-05-28 11:11:53 -0500

    Created starter file for transpiler text area editors

---
+++ jtp-client/src/components/TranspilerTextAreas.vue
@@ -0,0 +1,12 @@
+<script>
+
+export default {
+    data() {
+        return {
+            javaEditorDefualtText:  `public class Main {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello, World!");\n\t}\n}`,
+            pythonEditorDefaultText: `class Main:\n\tdef main(args):\n\t\tprint("Hello, World!")`,
+        }
+    }
+}
+
+</script>
